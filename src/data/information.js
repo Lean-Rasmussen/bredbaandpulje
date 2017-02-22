9bb67e69534c2241bb69d9c1857eb3120dc14d7a
Lean Rasmussen <leanmkrasmussen@gmail.com>
2017-02-22 10:18:08 +0100

    New folder made to contain data one for projekt only, one for generic information, and one for dynamic information. Router is getting setup index.js

---
+++ src/data/information.js
@@ -0,0 +1,5 @@
+const information = [
+    	'Vi har nu sendt klistermarker ud til jer', 
+    	'tilmeldings er begyndt for dem som ikke er med i projektet'
+
+    	],
--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
+import {Router, Route, hashHistory} from 'react-router';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+//Components
+import App from './App';
+import Information from './components/information'
+import Tilmelding from './components/tilmelding'
+import Progress from './components/progress'
+
+
+ReactDOM.render((
+	<Router history={hashHistory}>
+		<Route path="/" component={App}> 
+			<Route path="/information" component={Information} /> 
+			<Route path="/tilmelding" component={Tilmelding} /> 
+			<Route path="/progress" component={Progress} /> 
+		</Route>
+	</Router>
+),document.getElementById('root')
 );
